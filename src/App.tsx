4b6b4b059ea4a96843e0e3298b64a648015258db
yamori-masato <yamorimasato@gmail.com>
2021-03-04 22:04:30 +0900

    Navコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Row } from './components/Row'
 import { Banner } from './components/Banner'
+import { Nav } from './components/Nav'
 import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGUINALS"
