9fde95514bea1f38756f923eab1787d01bf91d3e
yamori-masato <yamorimasato@gmail.com>
2021-03-04 22:57:20 +0900

    トレイラーを表示

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import YouTube from 'react-youtube'
 import axios from './../axios'
 import "./Row.scss"
 
@@ -19,8 +20,17 @@ type Movie = {
   backdrop_path: string
 }
 
+type Options = {
+  height: string
+  width: string
+  playerVars: {
+    autoplay: 0 | 1 | undefined
+  }
+}
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [trailerUrl, setTrailerUrl] = useState<string | null>('')
 
   useEffect(() => {
     async function fetchData() {
@@ -30,8 +40,26 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     }
     fetchData()
   }, [fetchUrl])
-
   console.log(movies)
+
+  const opts: Options = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1
+    }
+  }
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+      )
+      setTrailerUrl(trailerurl.data.results[0]?.key)
+    }
+  }
 
   return (
     <div className='Row'>
@@ -46,9 +74,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
