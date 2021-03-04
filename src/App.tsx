1adedd5e4b6df9d4101649ea07ed038818a5882e
yamori-masato <yamorimasato@gmail.com>
2021-03-04 21:49:10 +0900

    Row,Bannerコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { Row } from './components/Row'
+import { Banner } from './components/Banner'
+import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGUINALS"
+        fetchUrl={requests.feachNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
-  );
+  )
 }
 
 export default App;
